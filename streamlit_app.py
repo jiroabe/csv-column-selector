033d2d732883a02a737bc134b7c158087e194d37
jiroabe <120996916+jiroabe@users.noreply.github.com>
2024-01-05 20:07:39 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,22 +10,27 @@ uploaded_file = st.file_uploader("Choose a CSV file", type='csv')
 # 列の選択を行う
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-    all_columns = df.columns.tolist()
-    # 初期状態で列が選択されていない状態にする
-    selected_columns = st.multiselect('Select columns', all_columns)
+
+    # テキスト入力で列名を入力する
+    columns_input = st.text_input('Enter column names separated by commas (e.g., サービス日付,曜日)')
 
     if st.button('Execute'):
+        # 入力された列名をカンマで分割してリストにする
+        selected_columns = [column.strip() for column in columns_input.split(',') if column.strip() in df.columns]
+
         # 選択された列のみを含む新しいDataFrameを作成
-        new_df = df[selected_columns]
+        if selected_columns:
+            new_df = df[selected_columns]
+            # データフレームを表示
+            st.write(new_df)
 
-        # データフレームを表示
-        st.write(new_df)
-
-        # CSVとしてダウンロード可能にする
-        csv = new_df.to_csv(index=False).encode('utf-8')
-        st.download_button(
-            label="Download CSV",
-            data=csv,
-            file_name='selected_columns.csv',
-            mime='text/csv',
-        )
+            # CSVとしてダウンロード可能にする
+            csv = new_df.to_csv(index=False).encode('utf-8')
+            st.download_button(
+                label="Download CSV",
+                data=csv,
+                file_name='selected_columns.csv',
+                mime='text/csv',
+            )
+        else:
+            st.error("Please enter valid column names.")
