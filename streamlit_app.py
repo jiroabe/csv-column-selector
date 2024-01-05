dfe4207e4b8608e9395dd3da003a32c6ba9cbd8a
jiroabe <120996916+jiroabe@users.noreply.github.com>
2024-01-05 19:57:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # ストリームリットページのタイトル
-st.title('CSV Column Selector and Exporter')
+st.title('CSV Column Selector')
 
 # CSVファイルのアップロード
 uploaded_file = st.file_uploader("Choose a CSV file", type='csv')
@@ -11,41 +11,21 @@ uploaded_file = st.file_uploader("Choose a CSV file", type='csv')
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     all_columns = df.columns.tolist()
-    selected_columns = st.multiselect('Select columns', all_columns, default=all_columns[:min(10, len(all_columns))])
-
-    # 出力形式の選択
-    file_format = st.selectbox('Select output file format', ['CSV', 'JSON'])
+    # 初期状態で列が選択されていない状態にする
+    selected_columns = st.multiselect('Select columns', all_columns)
 
     if st.button('Execute'):
         # 選択された列のみを含む新しいDataFrameを作成
         new_df = df[selected_columns]
 
-        if file_format == 'CSV':
-            # CSVプレビューを表示
-            st.write(new_df)
+        # データフレームを表示
+        st.write(new_df)
 
-            # CSVとしてダウンロード可能にする
-            csv = new_df.to_csv(index=False).encode('utf-8')
-            st.download_button(
-                label="Download CSV",
-                data=csv,
-                file_name='selected_columns.csv',
-                mime='text/csv',
-            )
-        elif file_format == 'JSON':
-            # JSON形式に変換（Unicodeエスケープを防ぐ）
-            json_data = new_df.to_json(orient='records', lines=False, indent=2, ensure_ascii=False)
-
-            # JSONデータをUTF-8でエンコード
-            json_data_encoded = json_data.encode('utf-8')
-
-            # JSONプレビューを表示（UTF-8でデコード）
-            st.text_area('JSON Preview', json_data, height=300)
-
-            # JSONとしてダウンロード可能にする
-            st.download_button(
-                label="Download JSON",
-                data=json_data_encoded,
-                file_name='selected_columns.json',
-                mime='application/json',
-            )
+        # CSVとしてダウンロード可能にする
+        csv = new_df.to_csv(index=False).encode('utf-8')
+        st.download_button(
+            label="Download CSV",
+            data=csv,
+            file_name='selected_columns.csv',
+            mime='text/csv',
+        )
