95a2ab15adccf392796bd87ba75014655e9b125b
jiroabe <120996916+jiroabe@users.noreply.github.com>
2024-01-05 19:48:03 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,13 +36,17 @@ if uploaded_file is not None:
         elif file_format == 'JSON':
             # JSON形式に変換（Unicodeエスケープを防ぐ）
             json_data = new_df.to_json(orient='records', lines=False, indent=2, ensure_ascii=False)
+
+            # JSONデータをUTF-8でエンコード
+            json_data_encoded = json_data.encode('utf-8')
+
             # JSONプレビューを表示
             st.text_area('JSON Preview', json_data, height=300)
 
             # JSONとしてダウンロード可能にする
             st.download_button(
                 label="Download JSON",
-                data=json_data,
+                data=json_data_encoded,
                 file_name='selected_columns.json',
                 mime='application/json',
             )
